96ae2f43a9e2e1d778011d923ed9902046a88ca3
Eric <yalmancierkan1@gmail.com>
2022-11-02 10:06:40 +0300

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import Header from '../../components/header/Header';
+import Cards from "../../components/cards/Cards"
+import { HeaderText, HomeImg, ImgDiv } from './Home.style';
+import homeSvg from "../../assets/home.svg"
+const Home = () => {
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const APP_KEY = process.env.REACT_APP_APP_KEY;
+  const [query, setQuery] = useState("egg");
+  const [selectedMeal, setSelectedMeal] = useState("breakfast");
+  const [recipes, setRecipes] = useState(null);
+  const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+//  ? it is not necessary to define mealtypes here, but maybe in the following times I need to use it in home component
+   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+  const getData = async () => {
+    if (query) {
+      try {
+        const { data } = await axios(url);
+        setRecipes(data.hits);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      alert("Fill the Form");
+    }
+  };
+  console.log(recipes);
+   
+  return (
+    <div>
+      <Header setQuery={setQuery} setSelectedMeal={setSelectedMeal} mealType={mealType} getData={getData}  />
+      {!recipes && 
+      <ImgDiv>
+      <HomeImg src={homeSvg} />
+      </ImgDiv>
+      }
+      {recipes?.length === 0 && <HeaderText>the food can not be found</HeaderText> }
+      {recipes?.length > 0 && <Cards recipes={recipes} />}
+    
+    </div>
+  )
+}
+
+export default Home
